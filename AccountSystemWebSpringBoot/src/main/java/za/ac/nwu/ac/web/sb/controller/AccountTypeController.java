6b3c945926c74322baa4dbc2d55941b6729b4c13
Damian <damiantommy01@gmail.com>
2021-10-05 10:54:17 +0200

    springboot

---
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -0,0 +1,12 @@
+package za.ac.nwu.ac.web.sb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AccountTypeController {
+
+
+
+}
---
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/exception/RestServiceApplication.java
@@ -0,0 +1,11 @@
+package za.ac.nwu.ac.web.sb.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+    public static void main (String[] args){
+        SpringApplication.run(RestServiceApplication.class,args);
+    }
+}
